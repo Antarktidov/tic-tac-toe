9a478c37f3dad050ed034c2242fb47eaf9fdbe19
Antarktidov <96629481+Antarktidov@users.noreply.github.com>
2022-07-15 13:23:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-	function change(idzero) {
-	idzero.innerHTML = "0";
-	idzero.classList.remove("sharp");
-	idzero.classList.add("zero");
-	
-	const idix = document.querySelectorAll(".sharp");
-	console.log(idix);
-	const index = Math.floor(Math.random() * idix.length);
-	console.log(index);
-	const idixid = idix[index];
-	console.log(idixid);
-				
-	idixid.innerHTML = "X";
-	idixid.classList.remove("sharp");
-	idixid.classList.add("ix");
+function change(idzero) {
+	if (idzero.classList.contains("sharp") ) {		
+		idzero.innerHTML = "0";
+		idzero.classList.remove("sharp");
+		idzero.classList.add("zero");
+		
+		const idix = document.querySelectorAll(".sharp");
+		console.log(idix);
+		const index = Math.floor(Math.random() * idix.length);
+		console.log(index);
+		const idixid = idix[index];
+		console.log(idixid);
+					
+		idixid.innerHTML = "X";
+		idixid.classList.remove("sharp");
+		idixid.classList.add("ix");
+	}
 }
